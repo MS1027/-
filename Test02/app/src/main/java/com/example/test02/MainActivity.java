576b77810f0a9e81651058d501c8eb82136a72df
MS1027 <sinms1431@naver.com>
2022-09-27 16:16:47 +0900

    Create New File

---
+++ Test02/app/src/main/java/com/example/test02/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.test02;
+
+import static android.view.View.*;
+import static android.widget.Toast.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "버튼 클릭!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
