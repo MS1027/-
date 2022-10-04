4d5e1830fa02d42a444913136ab42d20627336c0
MS1027 <sinms1431@naver.com>
2022-10-04 17:25:09 +0900

    Create New File

---
+++ ProgressBar1/app/src/main/java/com/example/progressbar1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.progressbar1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mProgStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mProgStatus = (ProgressBar) findViewById(R.id.progStatus);
+    }
+
+    public void mOnClick(View v){
+        switch (v.getId()){
+            case R.id.btnDecrease1:
+                mProgStatus.setProgress(mProgStatus.getProgress() - 5);
+                break;
+            case R.id.btnIncrease1:
+                mProgStatus.setProgress(mProgStatus.getProgress() + 5);
+                break;
+            case R.id.btnDecrease2:
+                mProgStatus.setProgress(mProgStatus.getSecondaryProgress() - 5);
+                break;
+            case R.id.btnIncrease2:
+                mProgStatus.setProgress(mProgStatus.getSecondaryProgress() + 5);
+                break;
+        }
+    }
+}
